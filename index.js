c1a97470f841582f324341f6d79ffd7694e27253
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2022-03-20 00:36:53 -0600

    Funcion de dar vuelta a las cartas, llevar contador de movimientos y reiniciar el juego

--- index.js
+++ index.js
@@ -8,33 +8,77 @@ const Header = () =>{
   )
 }
 
-const Card = ({url, i}) =>{
+const Card = ({url, click, flipped, rotation}) =>{
+
   return(
-    <div>
-      <div onClick = {() => console.log('wooooi')} className='card' style = {{backgroundImage: `url(${url})`}}></div>
+    <div className = 'cardcontainer' style = {{transform:rotation}}>
+      <div onClick = {click} className='card' style = {{backgroundImage: `url(${url})`}}></div>
     </div>
   )
 
 }
 
 const Board = ({images}) =>{
-  const backy = './back.png'
+  const backy = './backk.jpg'
   const touches = React.useRef(0)
-  const ultima = React.useRef(null)
-  const penultima = React.useRef(null)
+  const last = React.useRef(null)
+  const secondtolast = React.useRef(null)
   const wincondition =React.useRef(0)
+  const i1 = React.useRef(0)
+  const i2 = React.useRef(0)
+  const counter = React.useRef(0)
+
+  const [flipped, setFlipped] = React.useState(images.map(() =>false))
+
+  const refresh = () =>{
+    window.location.reload(false)
+  }
+
+  const clickManger = (i) =>{
+    if(touches.current<2 && flipped[i]===false){
+      const newstates = [...flipped]
+      newstates[i] = true
+      setFlipped(newstates)
+      touches.current = touches.current+1
+      if(touches.current === 1){
+        last.current = images[i]
+        i1.current = i
+      }
+      if(touches.current === 2){
+        i2.current = i1.current
+        i1.current = i
+        secondtolast.current = last.current
+        last.current = images[i]
+        if(last.current === secondtolast.current){
+          console.log('se pudo')
+        }else{
+          setTimeout(()=>{
+            const actualstates = [...flipped]
+            actualstates[i1] = false
+            setFlipped(actualstates)
+            actualstates[i2] = false
+            setFlipped(actualstates)
+            console.log(actualstates)
+          }, 2000)
+        }
+        counter.current = counter.current+1
+        touches.current = 0
+        last.current, secondtolast.current = 0
+      }
+    }
+  }
   return(
     <div className='divtainer'>
       <div className='options'>
-        <div>
-          <h1>Reiniciar</h1>
+        <div onClick = {() => refresh()}>
+          Reiniciar
         </div>
         <div>
-          <h1>Contador:</h1>
+          Total de intentos:{counter.current}
         </div>
       </div>
       <div className = 'board'>
-        {images.map((url, i) => <Card url = {url} key = {i}/>)}
+        {images.map((url, i) => <Card rotation = {flipped[i] ? 'rotateY(180deg)' : 'rotateY(0deg)'} url = {flipped[i] ? url : backy} key = {i} click = {() => clickManger(i)}/>)}
       </div>
     </div>
   )
