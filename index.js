b001d5a0084e32ff7e7b32986fc6b7252711c1a9
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2022-03-22 12:58:21 -0600

    Memoria Funcional, contador funcional, reiniciar funcional

--- index.js
+++ index.js
@@ -18,14 +18,40 @@ const Card = ({url, click, flipped, rotation}) =>{
 
 }
 
-const Board = ({images}) =>{
+
+
+const Winnn = () =>{
+  const winmage = './Regular.jpg'
+  
+  const modal = React.useRef(false)
+
+  const ToggleModal = () =>{
+    modal.current = !modal.current
+  }
+
+  return(
+    <div>
+      <div className = 'modal'>
+        <div className = 'overlay'>
+          <div className = 'modal-content'>
+            <h2>¡Has Ganado!</h2>
+            <button className = 'close' onClick = {ToggleModal()}>Cerrar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Board = ({images, modal}) =>{
   const backy = './backk.jpg'
   const touches = React.useRef(0)
   const last = React.useRef(null)
   const secondtolast = React.useRef(null)
   const wincondition =React.useRef(0)
-  const i1 = React.useRef(0)
-  const i2 = React.useRef(0)
+  const [i1, seti1] = React.useState(0)
+  const [i2, seti2]= React.useState(0)
+  const win = React.useRef(false)
   const counter = React.useRef(0)
 
   const [flipped, setFlipped] = React.useState(images.map(() =>false))
@@ -35,42 +61,46 @@ const Board = ({images}) =>{
   }
 
   const clickManger = (i) =>{
-    if(touches.current<2 && flipped[i]===false){
+    if(touches.current<=2 && flipped[i]===false){
       const newstates = [...flipped]
       newstates[i] = true
       setFlipped(newstates)
       touches.current = touches.current+1
       if(touches.current === 1){
         last.current = images[i]
-        i1.current = i
+        seti1(i)
       }
       if(touches.current === 2){
-        i2.current = i1.current
-        i1.current = i
         secondtolast.current = last.current
         last.current = images[i]
         if(last.current === secondtolast.current){
-          console.log('se pudo')
+          wincondition.current = wincondition.current+1
+          touches.current = 0
+          counter.current = counter.current+1
+          console.log(wincondition.current)
+          if(wincondition.current === 8){
+            ToggleModal()
+
+          }
         }else{
           setTimeout(()=>{
-            const actualstates = [...flipped]
-            actualstates[i1] = false
-            setFlipped(actualstates)
-            actualstates[i2] = false
-            setFlipped(actualstates)
-            console.log(actualstates)
-          }, 2000)
+            touches.current = 0
+            setFlipped(([...oldState]) =>{
+              oldState[i1] = false
+              oldState[i] = false
+              counter.current = counter.current+1
+              last.current, secondtolast.current = null
+              return oldState
+            })
+          }, 1000)
         }
-        counter.current = counter.current+1
-        touches.current = 0
-        last.current, secondtolast.current = 0
       }
     }
   }
   return(
     <div className='divtainer'>
       <div className='options'>
-        <div onClick = {() => refresh()}>
+        <div className='restart' onClick = {() => refresh()}>
           Reiniciar
         </div>
         <div>
@@ -83,11 +113,6 @@ const Board = ({images}) =>{
     </div>
   )
 }
-
-
-
-
-
 
 const App = () =>{
   const imagenes = ['./anti_pops.png', './benson.png', './death.png', './mordecai.png', './musculoso.png', 'papaleta.png', './rigby.png', './skips.png', './anti_pops.png', './benson.png', './death.png', './mordecai.png', './musculoso.png', 'papaleta.png', './rigby.png', './skips.png'].sort(() =>0.5-Math.random())
